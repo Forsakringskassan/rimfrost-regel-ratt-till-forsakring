421e2451a16671c9a20af2b7cb376d9dff20f99a
rikrhen <riki.rhen@ductus.se>
2025-10-20 10:01:19 +0200

    REST API call

---
+++ src/main/java/api/Folkbokford.java
@@ -0,0 +1,17 @@
+package api;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(configKey = "rtf")
+@Path("/population_registration")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public interface Folkbokford
+{
+
+    @GET
+    @Path("/{social_security_nr}")
+    boolean isFolkbokf(@PathParam("social_security_nr") String social_security_nr);
+}
