23287f2cdae6b04261857f9ec649490b9aab7fad
Riki Rhen <riki.rhen@ductus.se>
2025-10-22 11:05:01 +0200

    Update PresentationVahRtfRequest.java
    more old comments

--- src/main/java/presentation/dto/PresentationVahRtfRequest.java
+++ src/main/java/presentation/dto/PresentationVahRtfRequest.java
@@ -9,20 +9,3 @@ public interface PresentationVahRtfRequest
 {
    String personnummer();
 }
-
-//public class PresentationVahRtfRequest
-//{
-//
-//   //   public UUID processId;
-//   public String pnr;
-//
-//   public PresentationVahRtfRequest()
-//   {
-//   }
-//
-//   public PresentationVahRtfRequest(UUID processId, String pnr)
-//   {
-//      //      this.processId = processId;
-//      this.pnr = pnr;
-//   }
-//}
