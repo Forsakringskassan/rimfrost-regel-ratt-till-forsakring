85e1b33e71f7460e56c3142394d831ce1f95ca65
Riki Rhen <riki.rhen@ductus.se>
2025-10-22 11:05:25 +0200

    Update PresentationVahRtfResponse.java
    last old comment

--- src/main/java/presentation/dto/PresentationVahRtfResponse.java
+++ src/main/java/presentation/dto/PresentationVahRtfResponse.java
@@ -9,38 +9,3 @@ public interface PresentationVahRtfResponse
 {
    Boolean isBokford();
 }
-
-////   private UUID processId;
-//   private boolean result;
-//
-//   public PresentationVahRtfResponse()
-//   {
-//   }
-//
-//   public PresentationVahRtfResponse(UUID processId, boolean result)
-//   {
-////      this.processId = processId;
-//      this.result = result;
-//   }
-//
-////   public UUID getProcessId()
-////   {
-////      return processId;
-////   }
-////
-////   public void setProcessId(UUID processId)
-////   {
-////      this.processId = processId;
-////   }
-//
-//   public boolean isResult()
-//   {
-//      return result;
-//   }
-//
-//   public void setResult(boolean result)
-//   {
-//      this.result = result;
-//   }
-//
-//}
