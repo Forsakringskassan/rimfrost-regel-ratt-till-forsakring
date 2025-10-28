e6612130718b5a0004dbf2c3476298f2ddd01e4f
rikrhen <riki.rhen@ductus.se>
2025-10-28 13:34:40 +0100

    fix: deserializer please deserialize

--- src/main/java/se/fk/github/regelratttillforsakring/presentation/dto/VahRtfRequest.java
+++ src/main/java/se/fk/github/regelratttillforsakring/presentation/dto/VahRtfRequest.java
@@ -1,9 +1,12 @@
 package se.fk.github.regelratttillforsakring.presentation.dto;
 
 import java.util.UUID;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@JsonDeserialize(as = ImmutableVahRtfRequest.class)
 public interface VahRtfRequest
 {
    UUID processId();
