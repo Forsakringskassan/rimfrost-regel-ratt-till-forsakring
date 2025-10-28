e61991640e825f6ef53fd8c338f0a605e8e0cfab
rikrhen <riki.rhen@ductus.se>
2025-10-28 13:41:23 +0100

    fix: spotless

--- src/main/java/se/fk/github/regelratttillforsakring/kafka/VahRtfRequestDeserializer.java
+++ src/main/java/se/fk/github/regelratttillforsakring/kafka/VahRtfRequestDeserializer.java
@@ -3,8 +3,10 @@ package se.fk.github.regelratttillforsakring.kafka;
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 import se.fk.github.regelratttillforsakring.presentation.dto.VahRtfRequest;
 
-public class VahRtfRequestDeserializer extends ObjectMapperDeserializer<VahRtfRequest> {
-    public VahRtfRequestDeserializer() {
-        super(VahRtfRequest.class);
-    }
+public class VahRtfRequestDeserializer extends ObjectMapperDeserializer<VahRtfRequest>
+{
+   public VahRtfRequestDeserializer()
+   {
+      super(VahRtfRequest.class);
+   }
 }
