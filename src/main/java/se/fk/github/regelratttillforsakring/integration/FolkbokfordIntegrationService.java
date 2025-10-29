314b4b9b305adfa8553a9bf5b8856ff757964d6c
rikrhen <riki.rhen@ductus.se>
2025-10-29 15:39:17 +0100

    fix: integration package put back where it belongs

--- src/main/java/se/fk/github/regelratttillforsakring/integration/FolkbokfordIntegrationService.java
+++ src/main/java/se/fk/github/regelratttillforsakring/integration/FolkbokfordIntegrationService.java
@@ -1,13 +1,15 @@
 package se.fk.github.regelratttillforsakring.integration;
 
-import integration.dto.IntegrationFolkbokfordRequest;
-import integration.dto.IntegrationFolkbokfordResponse;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.fk.github.jaxrsclientfactory.JaxrsClientFactory;
 import se.fk.github.jaxrsclientfactory.JaxrsClientOptionsBuilders;
+import se.fk.github.regelratttillforsakring.integration.dto.IntegrationFolkbokfordRequest;
+import se.fk.github.regelratttillforsakring.integration.dto.IntegrationFolkbokfordResponse;
 import se.fk.rimfrost.api.folkbokforing.jaxrsspec.controllers.generatedsource.FolkbokforingControllerApi;
 
 @ApplicationScoped
@@ -19,6 +21,8 @@ public class FolkbokfordIntegrationService
 
    @Inject
    IntegrationMapper integrationMapper;
+
+   private static final Logger LOGGER = LoggerFactory.getLogger(FolkbokfordIntegrationService.class);
 
    private FolkbokforingControllerApi folkbokfordClient;
 
@@ -32,8 +36,11 @@ public class FolkbokfordIntegrationService
 
    public IntegrationFolkbokfordResponse checkFolkbokford(IntegrationFolkbokfordRequest externalRequest)
    {
+      LOGGER.info("Checking Folkbokford");
       var request = integrationMapper.toExternalApi(externalRequest);
+      LOGGER.info("Mapping to ExternalApi format done, sending request to: " + folkbokfordBaseUrl);
       var response = folkbokfordClient.folkbokforingPersnrGet(request);
+      LOGGER.info("Response from API was: " + response);
       return integrationMapper.fromExternalApi(response);
    }
 }
